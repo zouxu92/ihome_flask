2aa8102e666f83bf73c75f95debd1189b2a61af8
zouxu9203 <zouxu9203@163.com>
2019-08-27 15:33:30 +0800

    后端登录接口编写

--- ihome/api_1_0/passport.py
+++ ihome/api_1_0/passport.py
@@ -3,7 +3,7 @@
 from . import api
 from flask import request, jsonify, current_app, session
 from ihome.utils.response_code import RET
-from ihome import redis_store, db
+from ihome import redis_store, db, constants
 from ihome.models import User
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -95,3 +95,57 @@ def register():
 
 	# 返回结果
 	return jsonify(errno=RET.OK, errmsg="注册成功")
+
+@api.route("/sessions", methods=["POST"])
+def login():
+	"""登录
+	参数： 手机号、密码， json
+	"""
+	# 获取参数
+	req_dict = request.get_json()
+	mobile = req_dict.get("mobile")
+	password = req_dict.get("password")
+
+	# 检验参数
+	# 参数完整的效验
+	if not all([mobile, password]):
+		return jsonify(errno=RET.PARAMERR, errmsg="参数不完整")
+
+	# 手机号的格式
+	if not re.match(r"1[3456789]\d{9}", mobile):
+		return jsonify(errno=RET.PARAMERR, errmsg="手机号码格式错误")
+
+	# 判断错误次数，是否超过限制，如果操过限制，则返回
+	# redis 记录： "access_nums_请求的ip": "次数"
+	user_ip = request.remote_addr  # 用户的ip地址
+	try:
+		access_nums = redis_store.get("access_num_%s" % user_ip)
+	except Exception as e:
+		current_app.logger.error(e)
+	else:
+		if access_nums is not None and int(access_nums) >= constants.LOGIN_ERROR_MAX_TIMES:
+			return jsonify(errno=RET.REQERR, errmsg="错误次数过多，请稍后重试!")
+
+	# 从数据库中根据手机号码查询用户的数据对象
+	try:
+		user = User.query.filter_by(mobile=mobile).first()
+	except Exception as e:
+		current_app.logger.error(e)
+		return jsonify(errno=RET.DBERR, errmsg="获取用户信息失败")
+	# 用数据库的密码与用户填写的密码进行对比验证
+	if user is None or user.check_password(password):
+		# 如果验证码失败，记录错误次数， 返回信息
+		try:
+			redis_store.incr("access_num_%s" % user_ip)
+			redis_store.export("access_num_%s" % user_ip, constants.LOGIN_ERROR_FORBID_TIME)
+		except Exception as e:
+			current_app.logger.error(e)
+
+		return jsonify(errno=RET.DATAERR, errmsg="账号或密码错误")
+
+	# 如果验证相同成功，保存登录状态，在session中
+	session["name"] = user.name
+	session["mobile"] = user.mobile
+	session["user_id"] = user.id
+
+	return jsonify(errno=RET.OK, errmsg="登录成功")
