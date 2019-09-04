6933ccc9cd60e3d63a105fbcc174de012c6c9068
zouxu9203 <zouxu9203@163.com>
2019-09-04 11:54:09 +0800

    上传头像到七牛，并保存在数据库，返回上传的图片

--- ihome/constants.py
+++ ihome/constants.py
@@ -15,3 +15,6 @@ LOGIN_ERROR_MAX_TIMES = 5
 
 # 登录错误限制的时间， 单位：秒
 LOGIN_ERROR_FORBID_TIME = 600
+
+# 七牛的域名
+QINIU_URL_DOMAIN = "http://px92we95r.bkt.clouddn.com/"
--- ihome/utils/image_storage.py
+++ ihome/utils/image_storage.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# flake8: noqa
+
 from qiniu import Auth, put_data, etag
 import qiniu.config
 
@@ -11,20 +11,32 @@ secret_key = 'juQ7d6bKaJC-zAF4FSNLywo0gMf8rlyHZNu9HJhI'
 def storage(file_data):
 	"""
 	上传文件到七牛
-	:param file_data:
+	:param file_data: 要上传的文件数据
 	:return:
 	"""
 	# 构建鉴权对象
 	q = Auth(access_key, secret_key)
 
 	# 要上传的空间
-	bucket_name = 'ihome'
+	bucket_name = 'ihome-python04'
 
 
 	# 生成上传 Token，可以指定过期时间等
 	token = q.upload_token(bucket_name, None, 3600)
 
 	ret, info = put_data(token, None, file_data)
-	print(info)
-	print("*"*15)
-	print(ret)
+	# print(info)
+	# print("*"*15)
+	# print(ret)
+	if info.status_code == 200:
+		# 表示上传成功，返回文件名
+		return ret.get("key")
+	else:
+		# 上传失败
+		raise Exception("上传七牛失败")
+
+
+if __name__ == '__main__':
+	with open("./1.png", "rb") as f:
+		file_data = f.read()
+		storage(file_data)
