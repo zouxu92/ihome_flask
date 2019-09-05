1f830a977a329662f5f64c153ca2fd58988e31a4
zouxu-Home <zouxu9203@163.com>
2019-09-06 00:38:49 +0800

    城区信息前端编写，以及前端模板的使用

--- ihome/static/js/ihome/newhouse.js
+++ ihome/static/js/ihome/newhouse.js
@@ -3,7 +3,22 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
+
 $(document).ready(function(){
-    // $('.popup_con').fadeIn('fast');
-    // $('.popup_con').fadeOut('fast');
+    // 向后端获取城区信息
+    $.get("/api/v1.0/areas", function (resp) {
+        if (resp.errno == "0") {
+            var areas = resp.data;
+            // for (i=0; i<areas.length; i++){
+            //     var area = areas[i];
+            //     $("#area-id").append('<option value="'+ area.aid + '">'+ area.name +'</option>');
+            // }
+            // 使用js模板
+            var html = template("areas-tmpl", {areas:areas})
+            $("#area-id").html(html);
+
+        } else{
+            alert(resp.errmsg);
+        }
+    }, "json")
 })
