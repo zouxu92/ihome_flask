067d3570d212be09d0fc1619cddf916ae654c72b
zouxu9203 <zouxu9203@163.com>
2019-09-05 16:40:54 +0800

    保存实名认证以及实名认证完成后的显示

--- ihome/api_1_0/profile.py
+++ ihome/api_1_0/profile.py
@@ -44,7 +44,6 @@ def set_user_avatar():
 
 	avatar_url = constants.QINIU_URL_DOMAIN + file_name
 	# 保存成功返回
-	print (avatar_url)
 	return jsonify(errno=RET.OK, errmsg="保存成功", data={"avatar_url": avatar_url})
 
 @api.route("/users/name", methods=["PUT"])
@@ -73,6 +72,7 @@ def change_user_name():
 		current_app.logger.error(e)
 		db.session.rollback()
 		return jsonify(errno=RET.DBERR, errmsg="设置用户错误")
+
 	# 修改session数据中的name字段
 	session["name"] = name
 	return jsonify(errno=RET.OK, errmsg="OK", data={"name": name})
@@ -91,4 +91,55 @@ def get_user_profile():
 
     if user is None:
         return jsonify(errno=RET.NODATA, errmsg="无效操作")
+
     return jsonify(errno=RET.OK, errmsg="OK", data=user.to_dict())
+
+
+@api.route("/users/auth", methods=["GET"])
+@login_required
+def get_user_auth():
+    """获取用户的实名认证信息"""
+    user_id = g.user_id
+
+    # 在数据库中查询信息
+    try:
+        user = User.query.get(user_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="获取用户实名信息失败")
+
+    if user is None:
+        return jsonify(errno=RET.NODATA, errmsg="无效操作")
+
+    return jsonify(errno=RET.OK, errmsg="OK", data=user.auth_to_dict())
+
+
+@api.route("/users/auth", methods=["POST"])
+@login_required
+def set_user_auth():
+    """保存实名认证信息"""
+    user_id = g.user_id
+
+    # 获取参数
+    req_data = request.get_json()
+    if not req_data:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    real_name = req_data.get("real_name")  # 真实姓名
+    id_card = req_data.get("id_card")  # 身份证号
+
+    # 参数校验
+    if not all([real_name, id_card]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 保存用户的姓名与身份证号
+    try:
+        User.query.filter_by(id=user_id, real_name=None, id_card=None)\
+            .update({"real_name": real_name, "id_card": id_card})
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存用户实名信息失败")
+
+    return jsonify(errno=RET.OK, errmsg="OK")
