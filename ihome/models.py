cd193670d6fe0a9d9289d0aa044ea25b3b21598b
zouxu9203 <zouxu9203@163.com>
2019-09-05 16:42:10 +0800

    将获取用户信息和实名认证的信息转换成字典返回，在后端直接返回需要的字典数据

--- ihome/models.py
+++ ihome/models.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from datetime import datetime
-from . import db
+from . import db, constants
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -59,6 +59,25 @@ class User(BaseModel, db.Model):
         """
         return check_password_hash(self.password_hash, passwd)
 
+    def to_dict(self):
+        """将对象转换为字典数据"""
+        user_dict = {
+            "user_id": self.id,
+            "name": self.name,
+            "mobile": self.mobile,
+            "avatar": constants.QINIU_URL_DOMAIN + self.avatar_url if self.avatar_url else "",
+            "creat_time": self.create_time.strftime("%Y-%m-%d %H:%M:%S")
+        }
+        return user_dict
+
+    def auth_to_dict(self):
+        """将实名信息转换为字典数据"""
+        auth_dict = {
+            "user_id": self.id,
+            "real_name": self.real_name,
+            "id_card": self.id_card
+        }
+        return auth_dict
 
 class Area(BaseModel, db.Model):
     """城区"""
